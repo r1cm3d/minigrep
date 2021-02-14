30d51136454dc4850d2c5ea6cc88dcd0a916fec0
ricardomedeirosdacostajunior <ricardo.medeiros@pismo.io>
2021-02-14 13:31:06 -0300

    Revert "Improve run method using iterators"
    This reverts commit af22bbd492621fd9e77b6bff2fd822a13e623d4f.
    
    Signed-off-by: ricardomedeirosdacostajunior <ricardo.medeiros@pismo.io>

--- src/lib.rs
+++ src/lib.rs
@@ -9,19 +9,13 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
-        args.next();
+    pub fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
 
-        let query = match args.next() {
-            Some(arg) => arg,
-            None => return Err("Did not get a string query")
-        };
-
-        let filename = match args.next() {
-            Some(arg) => arg,
-            None => return Err("Did not get filename query")
-        };
-
+        let query = args[1].clone();
+        let filename = args[2].clone();
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
         Ok(Config { query, filename, case_sensitive })
@@ -58,7 +52,7 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    contens
+    contents
         .lines()
         .filter(|l| l.to_lowercase().contains(&query.to_lowercase()))
         .collect()
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let config = Config::new(env::args().collect()).unwrap_or_else(|err| {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
