7fe90f921aece99b28c61bb0ca9299aa18f1cd73
ricardomedeirosdacostajunior <ricardo.medeiros@pismo.io>
2021-02-14 00:48:09 -0300

    Add CASE_INSENSITIVE environment variable
    This patch add this environment variable configuration to case sensitive
    control of minigrep.
    
    Signed-off-by: ricardomedeirosdacostajunior <ricardo.medeiros@pismo.io>

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
 use std::error::Error;
-use std::fs;
+use std::{fs, env};
 
 #[derive(Debug)]
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -15,8 +16,9 @@ impl Config {
 
         let query = args[1].clone();
         let filename = args[2].clone();
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
-        Ok(Config { query, filename })
+        Ok(Config { query, filename, case_sensitive })
     }
 }
 
@@ -29,7 +31,13 @@ impl PartialEq for Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{}", line);
     }
 
@@ -48,26 +56,52 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents))
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "RuSt";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents))
     }
 
     #[test]
     fn config_new() {
         let args1 = &[String::from("minigrep"), String::from("query"), String::from("filename")][..];
         let args2 = &[String::from("filename")][..];
-        let cases = vec![("ok", args1, Ok(Config{ query: String::from("query"), filename: String::from("filename")})),
+        let cases = vec![("ok", args1, Ok(Config{ query: String::from("query"), filename: String::from("filename"), case_sensitive: false})),
                          ("error", args2, Err("not enough arguments"))];
 
         for case in cases {
